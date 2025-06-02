99116d33e77756409ff13da787077336d2e7dc50
Nafa06 <naufalanggardi09@gmail.com>
2025-06-02 14:38:07 +0700

    menambahkan fungsi main

--- Enkapsulasi.cpp
+++ Enkapsulasi.cpp
@@ -1,11 +1,18 @@
 #include <iostream>
 using namespace std;
 
-class remoteLampu{
-    private:
+class remoteLampu
+{
+private:
     string saklarNo[10];
+
 public:
-void setSaklarNo(int i, string value){
-    saklarNo[i] = value;
-}
+    void setSaklarNo(int i, string value)
+    {
+        saklarNo[i] = value;
+    }
 };
+
+int main(){
+    
+}
